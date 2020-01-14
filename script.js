720346f8a37c09ece9ff7f08a4b41ee6960172f1
Vishnu Gunapathi <38277626+Twinji@users.noreply.github.com>
2020-01-14 20:44:41 +1030

    Create initial prototype

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// window dimensions
+const WIDTH = window.innerWidth, 
+      HEIGHT = window.innerHeight;
+
 // point data type
 var Vector2 = function(x, y) {
   this.x = x;
@@ -5,11 +9,20 @@ var Vector2 = function(x, y) {
 }
 
 // firework particle data
-var maxFireworkParticles = 50;
-var fireworkParticlePositions;
-var fireworkParticleVelocities;
-var fireworkParticleLifespans;
-var gravityAcc = 2;
+var particlePositions;
+var particleVelocities;
+var particleLifespans;
+
+// limits
+var maxFireworkParticles = 1000;
+var maxParticleCountPerExplosion = 200;
+var minParticleCountPerExplosion = 100;
+var minParticleSlotsFreeBeforeSpawn = 20;
+var maxParticleVelocity = 4;
+var minParticleVelocity = 1;
+
+// environment
+var gravityAcc = 0.02;
 
 window.onload = function(e) {
 
@@ -34,12 +47,104 @@ window.onload = function(e) {
 function init(c) {
 
   // initialize firework particle data
-  fireworkParticlePositions = new Array(maxFireworkParticles);
-  fireworkParticleVelocities = new Array(maxFireworkParticles);
-  fireworkParticleLifespans = new Array(maxFireworkParticles);
+  particlePositions = new Array(maxFireworkParticles);
+  particleVelocities = new Array(maxFireworkParticles);
+  particleLifespans = new Array(maxFireworkParticles);
 
 }
 
-function update() {}
+function update() {
 
-function render(c) {}
+  // iterate over particles
+  for (var i = 0; i < maxFireworkParticles; i++) {
+
+    if (particlePositions[i] != null) {
+
+      // check if particle needs to be destroyed
+      if (particleLifespans[i] <= 0) {
+        
+        particlePositions[i] = null;
+        particleVelocities[i] = null;
+        particleLifespans[i] = null;
+        
+      } else {
+
+        // update positioning using velocity values
+        particlePositions[i].x += particleVelocities[i].x;
+        particlePositions[i].y -= particleVelocities[i].y;
+
+        // update velocity based on gravity
+        particleVelocities[i].y -= gravityAcc;
+
+        // decrement lifespan
+        particleLifespans[i]--;
+
+      }
+    }
+  }
+
+  // count empty particle slots
+  var numberOfEmptySlots = 0;
+  for (var i = 0; i < maxFireworkParticles; i++) {
+    if (!particlePositions[i]) numberOfEmptySlots++;
+  }
+
+  // spawn new particles if above limit
+  if (numberOfEmptySlots >= minParticleSlotsFreeBeforeSpawn) {
+
+    var numberToSpawn = Math.round(Math.random() * (maxParticleCountPerExplosion > numberOfEmptySlots ? numberOfEmptySlots : maxParticleCountPerExplosion));
+    var slotsToPopulate = new Array();
+
+    for (var i = 0; i < numberToSpawn; i++) {
+      if (!particlePositions[i]) slotsToPopulate.push(i);
+    }
+
+    var positionX = Math.random() * WIDTH;
+    var positionY = Math.random() * HEIGHT;
+
+    for (var i = 0; i < slotsToPopulate.length; i++) {
+
+      particlePositions[slotsToPopulate[i]] = new Vector2(
+        positionX,
+        positionY
+      );
+
+      var angle = Math.PI * 2 * (i / slotsToPopulate.length);
+
+      particleVelocities[slotsToPopulate[i]] = new Vector2(
+        Math.cos(angle) * (minParticleVelocity + Math.random() * (maxParticleVelocity - minParticleVelocity)),
+        Math.sin(angle) * (minParticleVelocity + Math.random() * (maxParticleVelocity - minParticleVelocity))
+      );
+
+      particleLifespans[slotsToPopulate[i]] = 100;
+
+    }
+
+  }
+
+}
+
+function render(c) {
+
+  // screen fade out effect
+  c.fillStyle = "black";
+  c.globalAlpha = 0.05;
+  c.fillRect(0, 0, WIDTH, HEIGHT);
+  c.globalAlpha = 1;
+
+  c.fillStyle = "white";
+  for (var i = 0; i < maxFireworkParticles; i++) {
+
+    if (particlePositions[i]) {
+
+      c.globalAlpha = Math.min(1, particleLifespans[i] / 60);
+      c.beginPath();
+      c.arc(particlePositions[i].x, particlePositions[i].y, Math.min(10, particleLifespans[i] / 60), 0, 2 * Math.PI);
+      c.fill();
+      c.closePath();
+      c.globalAlpha = 1;
+
+    }
+  }
+
+}
